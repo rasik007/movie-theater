71ed86fe794902f52a651ae8ef9fc7d5dc3276dc
rasik <rasik.prajapati@gmail.com>
2022-09-29 17:33:07 -0400

    updated based on requirement

--- src/main/java/com/jpmc/theater/MovieTheaterServiceApplication.java
+++ src/main/java/com/jpmc/theater/MovieTheaterServiceApplication.java
@@ -2,7 +2,9 @@ package com.jpmc.theater;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @Slf4j
--- src/main/java/com/jpmc/theater/controller/MovieScheduleController.java
+++ src/main/java/com/jpmc/theater/controller/MovieScheduleController.java
@@ -1,12 +1,15 @@
 package com.jpmc.theater.controller;
 
 import com.jpmc.theater.model.Showing;
+import com.jpmc.theater.service.MovieFeeCalculationService;
 import com.jpmc.theater.service.TheaterScheduleService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @RestController
@@ -16,11 +19,30 @@ public class MovieScheduleController {
     @Autowired
     private TheaterScheduleService theaterScheduleService;
 
+    @Autowired
+    private MovieFeeCalculationService movieFeeCalculationService;
+
     @GetMapping("/movieSchedule")
     public List<Showing> getMovieSchedule() {
         log.info("returning movie schedule");
         return theaterScheduleService.getMovieSchedule();
     }
 
+    @GetMapping("/movieScheduleInText")
+    public String getMovieScheduleInText() {
+        log.info("returning movie schedule");
+
+        StringBuilder sb = new StringBuilder();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+
+        theaterScheduleService.getMovieSchedule().forEach(s ->
+                sb.append("\n")
+                .append("MovieName :").append(s.getMovie().getTitle())
+                        .append(" Price: ").append(movieFeeCalculationService.calculateTicketPrice(s)).append(" USD")
+                        .append(" startTime :").append(s.getShowStartTime().format(formatter)).append(" RunningTime: ").append(s.getMovie().getRunningTime().toMinutes()).append(" mins").append("\n")
+        );
+        return sb.toString();
+    }
 
 }
